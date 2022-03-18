8865b4b3a9cbbe90d61d6c7245e9eb821a59d8ae
dropot <daniel.ropot@namics.com>
2022-03-18 12:36:26 +0100

    frontend: transform nav into a component with the help of js

---
+++ frontend/js/components/nav.js
@@ -0,0 +1,13 @@
+const printHtml = () => {
+    const nav = document.createElement("nav");
+    nav.setAttribute("class", "navbar")
+    nav.innerHTML = `
+        <img src="img/avataaars.png" width="60" height="60" class="logo" alt=""/>
+        <ul class="link-container">
+            <li class="link-item"><a href="/home.html" class="link">home</a></li>
+            <li class="link-item"><a href="/editor.html" class="link">editor</a></li>
+        </ul>
+    `;
+    document.querySelector("body").append(nav);
+}
+printHtml();
