ac6a0e2d8acb22116bb67ccfca93bc99d00b390b
surgeBinded <ropotdaniel@gmail.com>
2022-03-18 10:05:24 +0100

    initial commit

---
+++ src/main/kotlin/com/ropotdaniel/full_blog/FullBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.ropotdaniel.full_blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FullBlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<FullBlogApplication>(*args)
+}
---
+++ src/test/kotlin/com/ropotdaniel/full_blog/FullBlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ropotdaniel.full_blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FullBlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
