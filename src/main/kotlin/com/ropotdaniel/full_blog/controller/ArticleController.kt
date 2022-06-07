f257efec9b6347f7c253b9124d26e8150595405b
surgeBinded <ropotdaniel@gmail.com>
2022-06-07 09:01:49 +0200

    BE: small refactoring

--- src/main/kotlin/com/ropotdaniel/full_blog/controller/ArticleController.kt
+++ src/main/kotlin/com/ropotdaniel/full_blog/controller/ArticleController.kt
@@ -5,7 +5,7 @@ import com.ropotdaniel.full_blog.service.ArticleService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
-@CrossOrigin(origins = ["http://127.0.0.1:5500"])
+@CrossOrigin(origins = ["http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://127.0.0.1:5502"])
 @RestController
 @RequestMapping("/api/v1")
 class ArticleController @Autowired constructor(private val articleService: ArticleService) {
@@ -14,16 +14,10 @@ class ArticleController @Autowired constructor(private val articleService: Artic
     fun getArticles(): List<Article> = articleService.getAllArticles()
 
     @PostMapping("/article")
-    fun createArticle(
-        @RequestBody article: Article
-    ): Article {
-        return articleService.createArticle(article)
-    }
+    fun createArticle(@RequestBody article: Article): Article = articleService.createArticle(article)
 
     @PutMapping("/article/{id}")
-    fun updateArticle(@PathVariable id: Long, @RequestBody article: Article): Article{
-        return articleService.updateArticle(id, article)
-    }
+    fun updateArticle(@PathVariable id: Long, @RequestBody article: Article): Article = articleService.updateArticle(id, article)
 
     @DeleteMapping("/article/{id}")
     fun deleteArticle(@PathVariable id: Long) = articleService.deleteArticle(id)
