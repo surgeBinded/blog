6736ace0cdead00405a93ee78a54e92baf4a824c
surgeBinded <ropotdaniel@gmail.com>
2022-06-07 13:31:44 +0200

    FE: make possible to open published article on blog.html page

---
+++ frontend/js/blog.js
@@ -0,0 +1,23 @@
+const baseUrl = "http://localhost:8080/api/v1";
+const articles = "/articles";
+
+const para = new URLSearchParams(window.location.search);
+const id = para.get("ID");
+
+const fetchArticle = () => {
+    fetch(baseUrl + articles + "/" + id)
+        .then(response => response.json())
+        .then(element => {
+                document.getElementById("articleTitle").innerText = element.title;
+                document.getElementById("articlePublishDate").innerText = element.dateCreated;
+                document.getElementById("articleContent").innerText = element.content;
+                const image = document.createElement("img")
+                image.setAttribute("class", "banner")
+                image.setAttribute("id", "output")
+                image.setAttribute("src", `${element.bannerImage ? element.bannerImage : 'img/aerial-background-blog-cafe.jpg'}`)
+
+                document.getElementById("articleBanner").append(image);
+        })
+}
+
+fetchArticle();
