8a109c756e0858d57d84a3fcbe76987a8f7b3b10
surgeBinded <ropotdaniel@gmail.com>
2024-05-06 19:58:35 +0200

    fix: fix tests

--- src/test/kotlin/com/ropotdaniel/full_blog/controller/ArticleControllerIntegrationTest.kt
+++ src/test/kotlin/com/ropotdaniel/full_blog/controller/ArticleControllerIntegrationTest.kt
@@ -14,7 +14,6 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.time.ZonedDateTime
 
