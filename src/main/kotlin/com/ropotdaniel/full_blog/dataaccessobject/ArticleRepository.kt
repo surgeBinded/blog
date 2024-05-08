4deef73dd9f3c09d1bfeb2116782c5ce35b95971
surgeBinded <ropotdaniel@gmail.com>
2024-05-08 13:44:41 +0200

    refactor: remove empty body

--- src/main/kotlin/com/ropotdaniel/full_blog/dataaccessobject/ArticleRepository.kt
+++ src/main/kotlin/com/ropotdaniel/full_blog/dataaccessobject/ArticleRepository.kt
@@ -3,5 +3,4 @@ package com.ropotdaniel.full_blog.dataaccessobject
 import com.ropotdaniel.full_blog.domainobject.ArticleDO
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ArticleRepository : JpaRepository<ArticleDO, Long> {
-}
+interface ArticleRepository : JpaRepository<ArticleDO, Long>
