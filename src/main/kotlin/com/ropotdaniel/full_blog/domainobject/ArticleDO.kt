2ec6e9742737fea7508da14c05d1563466d6be6d
surgeBinded <ropotdaniel@gmail.com>
2024-05-26 19:46:21 +0200

    refactor: add validation to ArticleDO and logging to ArticleServiceImpl methods

--- src/main/kotlin/com/ropotdaniel/full_blog/domainobject/ArticleDO.kt
+++ src/main/kotlin/com/ropotdaniel/full_blog/domainobject/ArticleDO.kt
@@ -1,15 +1,35 @@
 package com.ropotdaniel.full_blog.domainobject
 
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.lang.Nullable
 import java.time.ZonedDateTime
-import jakarta.persistence.*
 
 @Entity
 @Table(name = "article")
-class ArticleDO(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long,
-                @Column(nullable = false) var title: String,
-                @Lob @Column(nullable = false) var content: String,
-                @Nullable @Lob var bannerImageUrl: String,
-                @Column(nullable = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var dateCreated: ZonedDateTime = ZonedDateTime.now()
+class ArticleDO(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long,
+
+    @Column(nullable = false)
+    @field:NotBlank(message = "Title cannot be blank")
+    @field:Size(
+        max = 100,
+        message = "Title cannot exceed 100 characters"
+    ) var title: String,
+
+    @Lob
+    @Column(nullable = false)
+    var content: String,
+
+    @Nullable
+    @Lob
+    var bannerImageUrl: String,
+
+    @Column(nullable = false)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    var dateCreated: ZonedDateTime = ZonedDateTime.now()
 )
--- src/main/kotlin/com/ropotdaniel/full_blog/service/ArticleServiceImpl.kt
+++ src/main/kotlin/com/ropotdaniel/full_blog/service/ArticleServiceImpl.kt
@@ -2,6 +2,7 @@ package com.ropotdaniel.full_blog.service
 
 import com.ropotdaniel.full_blog.dataaccessobject.ArticleRepository
 import com.ropotdaniel.full_blog.domainobject.ArticleDO
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -9,29 +10,37 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class ArticleServiceImpl @Autowired constructor(private val articleRepository: ArticleRepository) : ArticleService {
 
+    private val logger = LoggerFactory.getLogger(ArticleServiceImpl::class.java)
+
     override fun getArticle(id: Long): ArticleDO {
+        logger.info("Fetching article with id: $id")
         return articleRepository.getReferenceById(id)
     }
 
     override fun getAllArticles(): MutableList<ArticleDO> {
+        logger.info("Fetching all articles")
         return articleRepository.findAll()
     }
 
+    @Transactional
     override fun createArticle(articleDO: ArticleDO): ArticleDO {
+        logger.info("Creating new article with title: ${articleDO.title}")
         return articleRepository.save(articleDO)
     }
 
     @Transactional
     override fun updateArticle(id: Long, modifiedArticleDO: ArticleDO): ArticleDO {
+        logger.info("Updating article with id: $id")
         val article = articleRepository.getReferenceById(id)
         article.bannerImageUrl = modifiedArticleDO.bannerImageUrl
         article.content = modifiedArticleDO.content
         article.dateCreated = modifiedArticleDO.dateCreated
-
         return article
     }
 
+    @Transactional
     override fun deleteArticle(id: Long) {
+        logger.info("Deleting article with id: $id")
         articleRepository.deleteById(id)
     }
 }
