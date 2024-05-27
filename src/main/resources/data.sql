b02c443d42660facd148caae96383f00ff7d0637
surgeBinded <ropotdaniel@gmail.com>
2024-05-27 14:01:47 +0200

    bug: fix issue with adding comments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,11 @@
-INSERT INTO article (title, content, bannerImageUrl, dateCreated) VALUES
-('First Article', 'This is the content of the first article', 'http://example.com/image1.jpg', CURRENT_TIMESTAMP);
+INSERT INTO article (title, content, banner_image_url, date_created) VALUES
+    ('First Article', 'This is the content of the first article', 'example.com/image1.jpg', CURRENT_TIMESTAMP);
 
-INSERT INTO article (title, content, bannerImageUrl, dateCreated) VALUES
-('Second Article', 'This is the content of the second article', 'http://example.com/image2.jpg', CURRENT_TIMESTAMP);
+INSERT INTO article (title, content, banner_image_url, date_created) VALUES
+    ('Second Article', 'This is the content of the second article', 'example.com/image2.jpg', CURRENT_TIMESTAMP);
 
-
-INSERT INTO comment (articleId, parentCommentId, content, likes, dislikes, deleted) VALUES
+INSERT INTO comment (article_id, parent_comment_id, content, likes, dislikes, deleted) VALUES
     (1, NULL, 'This is a comment on the first article', 0, 0, false);
 
-INSERT INTO comment (articleId, parentCommentId, content, likes, dislikes, deleted) VALUES
+INSERT INTO comment (article_id, parent_comment_id, content, likes, dislikes, deleted) VALUES
     (1, 1, 'This is a subcomment on the first comment', 0, 0, false);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,14 +2,14 @@ CREATE TABLE IF NOT EXISTS article (
                                        id INT AUTO_INCREMENT  PRIMARY KEY,
                                        title VARCHAR(250) NOT NULL,
     content TEXT NOT NULL,
-    bannerImageUrl VARCHAR(250) NOT NULL,
-    dateCreated TIMESTAMP
+    banner_image_url VARCHAR(250) NOT NULL,
+    date_created TIMESTAMP
     );
 
 CREATE TABLE IF NOT EXISTS comment (
                                        id INT AUTO_INCREMENT  PRIMARY KEY,
-                                       articleId INT NOT NULL,
-    parentCommentId INT,
+                                       article_id INT NOT NULL,
+    parent_comment_id INT,
     content TEXT NOT NULL,
     likes INT NOT NULL,
     dislikes INT NOT NULL,
