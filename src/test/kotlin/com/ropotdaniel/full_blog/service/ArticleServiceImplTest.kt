40042a055a18aa2a2ef5d21e3009232e7f67d1c3
surgeBinded <ropotdaniel@gmail.com>
2024-06-11 21:09:57 +0200

    fix test

--- src/test/kotlin/com/ropotdaniel/full_blog/service/ArticleServiceImplTest.kt
+++ src/test/kotlin/com/ropotdaniel/full_blog/service/ArticleServiceImplTest.kt
@@ -1,6 +1,7 @@
 package com.ropotdaniel.full_blog.service
 
 import com.ropotdaniel.full_blog.dataaccessobject.ArticleRepository
+import com.ropotdaniel.full_blog.datatransferobject.ArticleDTO
 import com.ropotdaniel.full_blog.domainobject.ArticleDO
 import com.ropotdaniel.full_blog.mapper.CommentMapper
 import org.hamcrest.CoreMatchers.`is`
@@ -8,6 +9,10 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito.mock
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import java.time.ZonedDateTime
 
 class ArticleServiceImplTest {
 
@@ -15,6 +20,7 @@ class ArticleServiceImplTest {
     private val commentMapper: CommentMapper = mock(CommentMapper::class.java)
     private val articleService: ArticleService = ArticleServiceImpl(articleRepository, commentMapper)
     private val articleDO: ArticleDO = mock(ArticleDO::class.java)
+    private val articleDTO: ArticleDTO = mock(ArticleDTO::class.java)
 
     @Test
     fun `should return article by id`() {
@@ -30,16 +36,35 @@ class ArticleServiceImplTest {
     }
 
     @Test
-    fun `should return all articles`() {
+    fun `should return all articles paginated and sorted by id`() {
         // given
-        val articles = mutableListOf(articleDO)
-        given(articleRepository.findAll()).willReturn(articles)
+        val articles = mock(Page::class.java) as Page<ArticleDO>
+        given(articles.content).willReturn(listOfArticles())
+        given(articleRepository.findAll(PageRequest.of(0, 10, Sort.by("id").ascending()))).willReturn(articles)
 
         // when
         val actualArticles = articleService.getAllArticles(0, 10, "id", "asc")
 
         // then
-        assertThat("Returned articles should be the same as the mocked ones", actualArticles, `is`(articles))
+        assertThat("Should return the correct id of the first article", actualArticles.content.get(0).id, `is`(1L))
+        assertThat("Should return the correct number of pages", actualArticles.totalPages, `is`(0))
+        assertThat("Should return the correct amount of articles", actualArticles.totalElements, `is`(0))
+        assertThat("Should return the correct page number", actualArticles.pageNo, `is`(0))
+        assertThat("Should return the correct page size number", actualArticles.pageSize, `is`(0))
+    }
+
+    private fun listOfArticles() : List<ArticleDO> {
+        val listOfArticleDO = mutableListOf<ArticleDO>()
+        for (i in 1..100) {
+            val articleDO = ArticleDO(
+                i.toLong(),
+                "Test Title $i",
+                "Test Content $i",
+                ""
+            )
+            listOfArticleDO.add(articleDO)
+        }
+        return listOfArticleDO
     }
 
     @Test
