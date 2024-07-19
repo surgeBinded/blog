efcb870028c151be8f814ea98d9f5d8750813040
surgeBinded <ropotdaniel@gmail.com>
2024-07-19 22:42:10 +0200

    feature: change db data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,20 @@
-INSERT INTO article (title, content, banner_image_url, date_created) VALUES
-    ('First Article', 'This is the content of the first article', 'example.com/image1.jpg', CURRENT_TIMESTAMP);
+-- Insert Users
+INSERT INTO users (first_name, last_name, username, password, email) VALUES
+    ('John', 'Doe', 'johndoe', 'password1', 'johndoe@example.com');
 
-INSERT INTO article (title, content, banner_image_url, date_created) VALUES
-    ('Second Article', 'This is the content of the second article', 'example.com/image2.jpg', CURRENT_TIMESTAMP);
+INSERT INTO users (first_name, last_name, username, password, email) VALUES
+    ('Jane', 'Smith', 'janesmith', 'password2', 'janesmith@example.com');
 
-INSERT INTO comment (article_id, parent_comment_id, content, likes, dislikes, deleted) VALUES
-    (1, NULL, 'This is a comment on the first article', 0, 0, false);
+-- Insert Articles
+INSERT INTO article (title, content, banner_image_url, date_created, user_id) VALUES
+    ('First Article', 'This is the content of the first article', 'example.com/image1.jpg', CURRENT_TIMESTAMP, 1);
 
-INSERT INTO comment (article_id, parent_comment_id, content, likes, dislikes, deleted) VALUES
-    (1, 1, 'This is a subcomment on the first comment', 0, 0, false);
+INSERT INTO article (title, content, banner_image_url, date_created, user_id) VALUES
+    ('Second Article', 'This is the content of the second article', 'example.com/image2.jpg', CURRENT_TIMESTAMP, 2);
+
+-- Insert Comments
+INSERT INTO comment (article_id, parent_comment_id, content, likes, dislikes, deleted, user_id) VALUES
+    (1, NULL, 'This is a comment on the first article', 0, 0, false, 1);
+
+INSERT INTO comment (article_id, parent_comment_id, content, likes, dislikes, deleted, user_id) VALUES
+    (1, 1, 'This is a subcomment on the first comment', 0, 0, false, 2);
