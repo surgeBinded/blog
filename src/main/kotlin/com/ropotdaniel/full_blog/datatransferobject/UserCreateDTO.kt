64a878eebd4e33b6256f0ea42b1e647ad4e9f922
surgeBinded <ropotdaniel@gmail.com>
2024-07-31 23:11:18 +0200

    feature: add DTO for creating user

---
+++ src/main/kotlin/com/ropotdaniel/full_blog/datatransferobject/UserCreateDTO.kt
@@ -0,0 +1,9 @@
+package com.ropotdaniel.full_blog.datatransferobject
+
+data class UserCreateDTO(
+    var firstName: String,
+    var lastName: String,
+    var username: String,
+    var email: String,
+    var password: String
+)
