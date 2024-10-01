11544fffc96a967eb1c2417a47fa79631b2a4ad9
surgeBinded <ropotdaniel@gmail.com>
2024-10-01 22:50:17 +0200

    feature: add response entity for a more explicit exception handling. Intermediary step.

--- src/main/kotlin/com/ropotdaniel/full_blog/controller/AuthorController.kt
+++ src/main/kotlin/com/ropotdaniel/full_blog/controller/AuthorController.kt
@@ -6,6 +6,8 @@ import com.ropotdaniel.full_blog.datatransferobject.author.AuthorDTO
 import com.ropotdaniel.full_blog.service.AuthorService
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -13,16 +15,26 @@ import org.springframework.web.bind.annotation.*
 class AuthorController @Autowired constructor(private val authorService: AuthorService) {
 
     @GetMapping("/authors/{id}")
-    fun getAuthor(@PathVariable id: Long): AuthorDTO = authorService.getAuthor(id)
+    fun getAuthor(@PathVariable id: Long): ResponseEntity<AuthorDTO>
+        = ResponseEntity.ok(authorService.getAuthor(id))
 
     @PostMapping("/authors/register")
-    fun registerAuthor(@Valid @RequestBody createAuthorDTO: CreateAuthorDTO): AuthorDTO =
-        authorService.registerAuthor(createAuthorDTO)
+    fun registerAuthor(@Valid @RequestBody createAuthorDTO: CreateAuthorDTO): ResponseEntity<AuthorDTO>
+        = ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(authorService.registerAuthor(createAuthorDTO))
 
+    /*
+       TODO: only the admin and the author themselves should be able to execute the update here
+        perhaps consider creating a dedicated custom permission evaluator for authors
+     */
     @PatchMapping("/authors/{id}")
-    fun updateAuthor(@PathVariable id: Long, @RequestBody updateAuthorDTO: UpdateAuthorDTO): AuthorDTO =
-        authorService.updateAuthor(id, updateAuthorDTO)
+    fun updateAuthor(@PathVariable id: Long, @RequestBody updateAuthorDTO: UpdateAuthorDTO): ResponseEntity<AuthorDTO>
+        = ResponseEntity.ok(authorService.updateAuthor(id, updateAuthorDTO))
 
     @DeleteMapping("/authors/{id}")
-    fun deleteAuthor(@PathVariable id: Long) = authorService.deleteAuthor(id)
+    fun deleteAuthor(@PathVariable id: Long): ResponseEntity<String> {
+        authorService.deleteAuthor(id)
+        return ResponseEntity.ok("Author successfully deleted.");
+    }
 }
